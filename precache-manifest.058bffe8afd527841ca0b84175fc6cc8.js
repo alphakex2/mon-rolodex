9991eb7533491350cb5ebd509409728c3b6efed0
Kenny D <alphakex2@gmail.com>
2020-03-24 11:50:14 +0300

    Updates

---
+++ precache-manifest.058bffe8afd527841ca0b84175fc6cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9532d0e6cfa2e782f7d801188ad3463a",
+    "url": "/mon-rolodex/index.html"
+  },
+  {
+    "revision": "9cc3e61bba628fd73831",
+    "url": "/mon-rolodex/static/css/main.45b6c6be.chunk.css"
+  },
+  {
+    "revision": "407477f87f6cf1bd0b13",
+    "url": "/mon-rolodex/static/js/2.74750437.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mon-rolodex/static/js/2.74750437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc3e61bba628fd73831",
+    "url": "/mon-rolodex/static/js/main.1c506bbc.chunk.js"
+  },
+  {
+    "revision": "f4bb3b97ec664c2ba6b6",
+    "url": "/mon-rolodex/static/js/runtime-main.06cae716.js"
+  }
+]);
